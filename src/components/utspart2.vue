7fe0c9b05b04917d58ee86ff12cd80025ec8a260
olo steven <olostevenpanggabean@student.uir.ac.id>
2025-05-04 12:36:33 +0700

    feat(core): initialize reactive state and data structure

--- src/components/utspart2.vue
+++ src/components/utspart2.vue
@@ -1,4 +1,20 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+
+// function selected day
+const days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'];
+const selectedDay = ref('Minggu');
+const filter = ref('all');
+const expenses = ref({});
+const newExpense = ref({
+  text: '',
+  amount: 0,
+  completed: false
+});
+
+days.forEach(day => {
+  expenses.value[day] = [];
+});
 
 </script>
 
